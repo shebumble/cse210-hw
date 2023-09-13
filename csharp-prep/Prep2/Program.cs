18957ed5baac98c17748eadcf0ed2a8f9583ccaf
Shelby Niver <shelbylalbrecht@gmail.com>
2023-09-13 16:14:13 -0600

    Replaced the hello world program with a grade checking program as instructed by prep 2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letterGrade = "";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80) 
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations on passing!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass, but you can do better next time!");
+        }
     }
 }
