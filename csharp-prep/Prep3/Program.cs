42200b5cb412afa4733ed5374dcda118f6dddbc7
Shelby Niver <shelbylalbrecht@gmail.com>
2023-09-18 14:05:42 -0600

    Replaced the code with the expected program for a number guessing game given by the instructions of prep #3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+
+        while (guess != magicNumber) 
+        {
+            Console.WriteLine("What is your guess? ");
+            string userInput2 = Console.ReadLine();
+            guess = int.Parse(userInput2);
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess == magicNumber)
+            {
+                Console.WriteLine("Your guessed it!");
+            }
+        }
     }
 }
