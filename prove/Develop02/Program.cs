8e8dc9d8928af4d31df6bc46b5ed518b3b0b2b8d
Shelby Niver <shelbylalbrecht@gmail.com>
2023-10-07 14:13:20 -0600

    created the main program file and function in order to run the journal assignment.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,64 @@
 using System;
 
-class Program
+/*To improve, I added an option to add a new prompt to the prompts list, 
+as well as made sure the program checked to see if a file even existed before it deleted the whole journal
+and tried to load a new one. As well added a time to the saved entry.
+*/
+
+int choice = -1;
+Journal journal = new Journal();
+Prompts prompts = new Prompts();
+string menu = @"Please choose a menu option: 
+            0. Quit
+            1. Write
+            2. Display
+            3. Save
+            4. Load
+            5. Add prompt";
+
+while (choice != 0)
 {
-    static void Main(string[] args)
+    // added an option to add a prompt choice to the prompts list.
+    Console.WriteLine(menu);
+
+    string userChoice = Console.ReadLine();
+    choice = int.Parse(userChoice);
+
+
+    if (choice == 1)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Entry entry = new Entry();
+
+        entry._date = DateTime.Now.ToString("MM/dd/yyyy");
+
+        entry._time = DateTime.Now.ToString("h:mm:ss tt");
+
+        entry._prompt = prompts.DisplayPrompt();
+
+        entry._entry = Console.ReadLine();
+
+        journal._entries.Add(entry);
+
+    }
+    else if (choice == 2)
+    {
+        journal.Display();
+    }
+    else if (choice == 3)
+    {
+        journal.SaveJournal();
+    }
+    else if (choice == 4)
+    {
+        journal.LoadJournal();
+    }
+    else if (choice == 5)
+    {
+        Console.WriteLine("What is the new prompt?");
+        prompts._prompts.Add(Console.ReadLine());
+    }
+    else if (choice == 0)
+    {
+        Console.WriteLine("Thank you, have a good one!");
     }
 }
