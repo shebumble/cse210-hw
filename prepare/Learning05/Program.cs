845d73800718586e153642e2233b7abbd6974469
Shelby Niver <shelbylalbrecht@gmail.com>
2023-11-07 11:08:45 -0700

    add shape classes as instructed by the prep lesson and tested them in main program according to the lesson.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s = new Square("Orange", 4);
+        shapes.Add(s);
+
+        Rectangle r = new Rectangle("Yellow", 2, 5);
+        shapes.Add(r);
+
+        Circle c = new Circle("Red", 8);
+        shapes.Add(c);
+
+        foreach (Shape x in shapes)
+
+        {
+            string color = x.GetColor();
+
+            double area = x.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
     }
 }
